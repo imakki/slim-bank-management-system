c95ec61b07c3cf6709e569513ca58a7630b6225d
Ankit <ankit.escalate360@gmail.com>
2020-09-14 19:55:57 +0530

    login redirecting and view completed for all routes

--- src/Bank/Controllers/Users/UsersAndBalanceController.php
+++ src/Bank/Controllers/Users/UsersAndBalanceController.php
@@ -17,11 +17,24 @@ class UsersAndBalanceController {
 
         //echo ("users balance");
 
-        $sql = "SELECT DISTINCT accountId  FROM accounts";
+        //$sql = "SELECT users.accountId, accounts.current_balance, accounts.date_of_transaction, accounts.transaction_number  FROM users RIGHT JOIN accounts ON users.accountId = accounts.accountId ORDER BY accounts.transaction_number";
+        $sql = "SELECT DISTINCT accountId,username from users";
 
         $result = $this->container->db->query($sql)->fetchAll(\PDO::FETCH_OBJ);
 
-        return $response->withJson($result,200);
+        $output = [];
+        //loop through distinct id array to fetch current balance of each user
+        foreach ($result as $key => $value) {
+            $res = $result[$key]->{'accountId'};
+            $CURRENT_BALANCE_QUERY = "SELECT current_balance FROM accounts where accountId = $res ORDER BY transaction_number DESC LIMIT 1";
+
+            $balance = $this->container->db->query($CURRENT_BALANCE_QUERY)->fetchAll(\PDO::FETCH_OBJ);
+            $value->balance = $balance[0]->{'current_balance'};
+            array_push($output, $value);
+
+        }
+        
+        return $response->withJson($output,200);
 
     }
 
